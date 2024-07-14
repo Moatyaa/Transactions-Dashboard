091c3caf152496b881d2be4aafad952f022f0295
Moatyaa <muhammadatyaa66@gmail.com>
2024-07-14 21:40:09 +0300

    final

---
+++ customer-api/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Data
+const data = {
+  customers: [
+    { id: 1, name: "Ahmed Ali" },
+    { id: 2, name: "Aya Elsayed" },
+    { id: 3, name: "Mina Adel" },
+    { id: 4, name: "Sarah Reda" },
+    { id: 5, name: "Mohamed Sayed" }
+  ],
+  transactions: [
+    { id: 1, customer_id: 1, date: "2022-01-01", amount: 1000 },
+    { id: 2, customer_id: 1, date: "2022-01-02", amount: 2000 },
+    { id: 3, customer_id: 2, date: "2022-01-01", amount: 550 },
+    { id: 4, customer_id: 3, date: "2022-01-01", amount: 500 },
+    { id: 5, customer_id: 2, date: "2022-01-02", amount: 1300 },
+    { id: 6, customer_id: 4, date: "2022-01-01", amount: 750 },
+    { id: 7, customer_id: 3, date: "2022-01-02", amount: 1250 },
+    { id: 8, customer_id: 5, date: "2022-01-01", amount: 2500 },
+    { id: 9, customer_id: 5, date: "2022-01-02", amount: 875 }
+  ]
+};
+
+// Middleware
+app.use(cors());
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
+// endPoints
+app.get('/', (req, res) => {
+  res.send("Hey I'm Running");
+});
+
+app.get('/customers', (req, res) => {
+  res.json(data.customers);
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(data.transactions);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
